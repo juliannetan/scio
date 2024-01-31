acd3a0600e44e22420fc874c29fddf72eeaad5e4
juliannetan <juliannemtan@gmail.com>
2024-01-31 14:21:18 -0700

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import styled, { ThemeProvider } from 'styled-components'
 import theme from './theme'
-import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import SignIn from './pages/SignInPage';
 import SignUp from './pages/SignUpPage';
 import AppBar from './components/AppBar';
@@ -29,13 +29,6 @@ const App = () => {
     sessionStorage.setItem('token', JSON.stringify(token))
   }
 
-  // useEffect(() => {
-  //   if (sessionStorage.getItem('token')) {
-  //     let data = JSON.parse(sessionStorage.getItem('token'))
-  //     setToken(data)
-  //   }
-  // }, [])
-
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const magicLinkToken = urlParams.get('token');
@@ -51,52 +44,13 @@ const App = () => {
     }
   }, []);
 
-  // const [token, setToken] = useState(null);
-
-  // useEffect(() => {
-  //   const sessionToken = sessionStorage.getItem('token');
-  //   if (sessionToken) {
-  //     let data = JSON.parse(sessionToken);
-  //     setToken(data);
-  //   }
-  // }, []);
-
-  // const handleEmailPasswordSignIn = async (email, password) => {
-  //   try {
-  //     const { user, error } = await supabase.auth.signInWithPassword({
-  //       email,
-  //       password,
-  //     });
-
-  //     if (error) {
-  //       console.error('Error signing in:', error.message);
-  //       return;
-  //     }
-
-  //     setToken(user);
-  //     sessionStorage.setItem('token', JSON.stringify(user));
-  //   } catch (error) {
-  //     console.error('Error signing in:', error.message);
-  //   }
-  // };
-
-  // const navigate = useNavigate(); // Move this inside the App component
 
   const handleMagicLinkSignIn = async (tokenData) => {
     setToken(tokenData);
     sessionStorage.setItem('token', JSON.stringify(tokenData));
-    // navigate(`${window.location.origin}/scio`);
         return <Navigate to="/scio/home" />;
 
   };
-
-  // const handleMagicLinkSignIn = async (tokenData) => {
-  //   setToken(tokenData);
-  //   sessionStorage.setItem('token', JSON.stringify(tokenData));
-  //   return <Navigate to="/scio/home" />;
-  // };
-
-  
 
   const handleSignOut = async () => {
     await supabase.auth.signOut();
@@ -109,10 +63,6 @@ const App = () => {
       <AppContainer>
       <Router>
           <Routes>
-            {/* <Route
-              path="/scio/"
-              element={<SignIn setToken={handleMagicLinkSignIn} onEmailPasswordSignIn={handleEmailPasswordSignIn} />}
-            /> */}
             <Route path="/scio/signup" element={<SignUp />} />
             <Route
               path="/scio/home"
@@ -120,7 +70,6 @@ const App = () => {
             />
             <Route path="/scio/forgot-password" element={<ForgotPasswordPage />} />
           <Route path="/scio/" element={<SignIn setToken={setToken} onMagicLinkSignIn={handleMagicLinkSignIn} />} />
-          {/* <Route path="/scio/" element={<AppBar />} />  */}
           <Route path="/scio/problem-statement" element={<ProblemblockPage />} /> 
           <Route path="/scio/current-state" element={<CurrentblockPage />} /> 
           <Route path="/scio/future-state" element={<FutureblockPage />} /> 
@@ -130,7 +79,7 @@ const App = () => {
           <Route path="/scio/value-delivery" element={<ValueblockPage />} /> 
           <Route path="/scio/lessons-learned" element={<LessonsblockPage />} /> 
           <Route path="/scio/title-block" element={<TitleblockPage />} /> 
-        {/* {token ? <Route path='/scio/MissionPage' element={<AppBar token={token} />} /> : 'NavigateToSignIn' } */}
+          <Route path="/scio/a3-canvas" element={<GlanceA3Canvas />} /> 
           </Routes>
       </Router>
       </AppContainer>
