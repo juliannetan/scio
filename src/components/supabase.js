210c6d57e5d4a5e3c36d96bdf683f7865a46ef53
Charlie1991 <juancarlosnbcc@gmail.com>
2024-02-02 09:25:17 -0700

    code to fetch data from supabase

--- src/components/supabase.js
+++ src/components/supabase.js
@@ -9,4 +9,4 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://vrkrxuzxtdbtcwyhcaiq.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZya3J4dXp4dGRidGN3eWhjYWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4NDE3NzIsImV4cCI6MjAyMDQxNzc3Mn0.wV5u-vapo96l-qs59WKtbhYgtofOtr89MxBkYM4oEoY'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
--- src/pages/GlanceA3Canvas.js
+++ src/pages/GlanceA3Canvas.js
@@ -2,6 +2,10 @@ import { Button } from '@mui/material';
 import React from 'react';
 import styled from "styled-components";
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { supabase } from '../components/supabase';
+// import { currencyPairs } from '@mui/x-data-grid-generator';
+
 
 
 
@@ -169,10 +173,10 @@ const Div15 = styled.div`
 
 const Div16 = styled.div`
   color: #000;
-  background-color: #d9d9d9;
+  background-color: none;
   aspect-ratio: 1.24;
   justify-content: center;
-  padding: 3px 14px;
+  padding: px 14px;
   font: 600 24px/130% Inter, sans-serif;
 `;
 
@@ -192,21 +196,21 @@ const Div18 = styled.div`
 `;
 
 const Div19 = styled.div`
-  display: flex;
-  justify-content: end;
+  display: block;
   color: #000;
-  white-space: nowrap;
+  border-radius:10px;
   background-color: #d9d9d9;
-  margin-top: 2px;
-  flex:
-  align-items: end;
-  padding: 250px 14px 11px 60px;
-  font: 400 12px/130% Inter, sans-serif;
+  margin-top: 1px;
+  padding: 20px 60px;
+  font: 400 20px/130% Inter, sans-serif;
+  ${'' /* white-space: nowrap; */}
+   ${'' /* justify-content: end;
+  align-items: end; */}
 `;
 
 const Div20 = styled.div`
   color: #000;
-  background-color: #d9d9d9;
+  background-color: none;
   aspect-ratio: 1.24;
   justify-content: center;
   padding: 3px 14px;
@@ -229,23 +233,216 @@ const Div22 = styled.div`
 `;
 
 const Div23 = styled.div`
+  display: block;
+  color: #000;
+  border-radius:10px;
+  background-color: #d9d9d9;
+  margin-top: 1px;
+  padding: 20px 60px;
+  font: 400 20px/130% Inter, sans-serif;
+`;
+
+const Div24 = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  width: 500px;
+  height: 200px;
+  font-size:15px;
+  word-wrap: break-word;
+`;
+
+const Div25 = styled.div`
   display: flex;
   justify-content: end;
-  color: #000;
-  white-space: nowrap;
-  background-color: #d9d9d9;
-  margin-top: 2px;
-  align-items: end;
-  padding: 250px 14px 11px 40px;
-  font: 400 12px/130% Inter, sans-serif;
 `;
 
 
 
 
-const A3Canvas=(props) => {
+const A3Canvas=() => {
+
+  const [fetchError,setFetchError ]=useState(null)
+  const [Problemcontent,setProblemcontent] = useState(null)
+  const [Currentcontent,setCurrentcontent]=useState(null)
+  const [Futurecontent,setFuturecontent]=useState(null)
+  const [Solutioncontent,setSolutioncontent]=useState(null)
+  const [Decisioncontent,setDecisioncontent]=useState(null)
+  const [Implementationcontent,setImplementationcontent]=useState(null)
+  const [Valuecontent,setValuecontent]=useState(null)
+  const [Lessonscontent,setLessonscontent]=useState(null)
+
+
+
+
+
+  useEffect(() => {
+    const fetchProblemcontent = async () => {
+      const { data, error } = await supabase
+      .from('Problemcontent')
+      .select('')
+      
+      if (error) {
+        setFetchError('Could not fetch the Current Content Table')
+        setProblemcontent(null)
+        console.log(error)
+      }
+      if (data){
+        setProblemcontent(data)
+        setFetchError(null)
+      }
+  }
+  fetchProblemcontent()
+  },[])
   
+
+  useEffect(() => {
+    const fetchCurrentcontent = async () => {
+      const { data, error } = await supabase
+      .from('Currentcontent')
+      .select('')
+
+      if (error) {
+        setFetchError('Could not fetch the Current Content Table')
+        setCurrentcontent(null)
+        console.log(error)
+      }
+      if (data){
+        setCurrentcontent(data)
+        setFetchError(null)
+      }
+  }
+  fetchCurrentcontent()
+},[])
+
+
+
+useEffect(() => {
+  const fetchFuturecontent = async () => {
+    const { data, error } = await supabase
+    .from('Futurecontent')
+    .select()
+
+    if (error) {
+      setFetchError('Could not fetch the Current Content Table')
+      setFuturecontent(null)
+      console.log(error)
+    }
+    if (data){
+      setFuturecontent(data)
+      setFetchError(null)
+    }
+}
+fetchFuturecontent()
+},[])
+
+
+useEffect(() => {
+  const fetchSolutioncontent = async () => {
+    const { data, error } = await supabase
+    .from('Solutioncontent')
+    .select()
+
+    if (error) {
+      setFetchError('Could not fetch the Current Content Table')
+      setSolutioncontent(null)
+      console.log(error)
+    }
+    if (data){
+      setSolutioncontent(data)
+      setFetchError(null)
+    }
+}
+fetchSolutioncontent()
+},[])
+
+
+
+useEffect(() => {
+  const fetchDecisioncontent = async () => {
+    const { data, error } = await supabase
+    .from('Decisioncontent')
+    .select()
+
+    if (error) {
+      setFetchError('Could not fetch the Current Content Table')
+      setDecisioncontent(null)
+      console.log(error)
+    }
+    if (data){
+      setDecisioncontent(data)
+      setFetchError(null)
+    }
+}
+fetchDecisioncontent()
+},[])
+
+
+useEffect(() => {
+  const fetchImplementationcontent = async () => {
+    const { data, error } = await supabase
+    .from('Implementationcontent')
+    .select('')
+    
+    if (error) {
+      setFetchError('Could not fetch the Current Content Table')
+      setImplementationcontent(null)
+      console.log(error)
+    }
+    if (data){
+      setImplementationcontent(data)
+      setFetchError(null)
+    }
+}
+fetchImplementationcontent()
+},[])
+
+
+
+useEffect(() => {
+  const fetchValuecontent = async () => {
+    const { data, error } = await supabase
+    .from('Valuecontent')
+    .select('')
+    
+    if (error) {
+      setFetchError('Could not fetch the Current Content Table')
+      setValuecontent(null)
+      console.log(error)
+    }
+    if (data){
+      setValuecontent(data)
+      setFetchError(null)
+    }
+}
+fetchValuecontent()
+},[])
+ 
+
+
+
+
+useEffect(() => {
+  const fetchLessonscontent = async () => {
+    const { data, error } = await supabase
+    .from('Lessonscontent')
+    .select('')
+    
+    if (error) {
+      setFetchError('Could not fetch the Current Content Table')
+      setLessonscontent(null)
+      console.log(error)
+    }
+    if (data){
+      setLessonscontent(data)
+      setFetchError(null)
+    }
+}
+fetchLessonscontent()
+},[])
   return (
+
+        
     <Div>
 
       <Div2>
@@ -267,24 +464,55 @@ const A3Canvas=(props) => {
 
 
       <Div15>
+
         <Div16>1</Div16>
         <Div17>
           <Div18>Problem Statement</Div18>
           <Div19 >
+          <Div24>
+              {fetchError && (<p>{fetchError}</p>)}
+              {Problemcontent && (
+            <div>
+              {Problemcontent.map(Problemcontent => (
+              <p>{Problemcontent.PS1}</p>
+              ))}
+            </div>
+             )}
+          </Div24>
+          <Div25>
           <Link to="/scio/problem-statement">
             <Button>More Info...</Button>
           </Link>
+          </Div25>
           </Div19>
         </Div17>
+
+
         <Div20>5</Div20>
         <Div21>
           <Div22>Decision</Div22>
           <Div23>
+          <Div24>
+              {fetchError && (<p>{fetchError}</p>)}
+              {Decisioncontent && (
+            <div>
+              {Decisioncontent.map(Decisioncontent => (
+              <p>{Decisioncontent.DS1}</p>
+              ))}
+            </div>
+             )}
+          </Div24>
+          <Div25>
           <Link to="/scio/decision">
             <Button>More Info...</Button>
           </Link>
+          </Div25>
+
+          
           </Div23>
         </Div21>
+
+
       </Div15>
 
       <Div15>
@@ -292,18 +520,43 @@ const A3Canvas=(props) => {
         <Div17>
           <Div18>Current State</Div18>
           <Div19 >
+          <Div24>
+              {fetchError && (<p>{fetchError}</p>)}
+              {Currentcontent && (
+            <div>
+              {Currentcontent.map(Currentcontent => (
+              <p>{Currentcontent.CS1}</p>
+              ))}
+            </div>
+             )}
+          </Div24>
+          <Div25>
           <Link to="/scio/current-state">
             <Button>More Info...</Button>
           </Link>
+          </Div25>
+          
           </Div19>
           </Div17>
         <Div20>6</Div20>
         <Div21>
           <Div22>Implementation Plan</Div22>
           <Div23>
+          <Div24>
+              {fetchError && (<p>{fetchError}</p>)}
+              {Implementationcontent && (
+            <div>
+              {Implementationcontent.map(Implementationcontent => (
+              <p>{Implementationcontent.IPQ1}</p>
+              ))}
+            </div>
+             )}
+          </Div24>
+          <Div25>
           <Link to="/scio/implementation-plan">
             <Button>More Info...</Button>
           </Link>
+          </Div25>
           </Div23>
         </Div21>
       </Div15>
@@ -313,18 +566,43 @@ const A3Canvas=(props) => {
         <Div17>
           <Div18>Future State</Div18>
           <Div19 >
+          <Div24>
+              {fetchError && (<p>{fetchError}</p>)}
+              {Futurecontent && (
+            <div>
+              {Futurecontent.map(Futurecontent => (
+              <p>{Futurecontent.FS1}</p>
+              ))}
+            </div>
+             )}
+          </Div24>
+          <Div25>
           <Link to="/scio/future-state">
             <Button>More Info...</Button>
           </Link>
+          </Div25>
+          
           </Div19>
         </Div17>
         <Div20>7</Div20>
         <Div21>
           <Div22>Value Delivery</Div22>
           <Div23>
+          <Div24>
+              {fetchError && (<p>{fetchError}</p>)}
+              {Valuecontent && (
+            <div>
+              {Valuecontent.map(Valuecontent => (
+              <p>{Valuecontent.VDQ1}</p>
+              ))}
+            </div>
+             )}
+          </Div24>
+          <Div25>
           <Link to="/scio/value-delivery">
             <Button>More Info...</Button>
           </Link>
+          </Div25>
           </Div23>
           </Div21>
       </Div15>
@@ -334,18 +612,43 @@ const A3Canvas=(props) => {
         <Div17>
           <Div18>Solution Evaluation</Div18>
           <Div19 >
+          <Div24>
+              {fetchError && (<p>{fetchError}</p>)}
+              {Solutioncontent && (
+            <div>
+              {Solutioncontent.map(Solutioncontent => (
+              <p>{Solutioncontent.SEQ1}</p>
+              ))}
+            </div>
+             )}
+          </Div24>
+          <Div25>
           <Link to="/scio/solution-evaluation">
             <Button>More Info...</Button>
           </Link>
+          </Div25>
+          
           </Div19>
         </Div17>
         <Div20>8</Div20>
         <Div21>
           <Div22>Lessons Learned</Div22>
           <Div23>
+          <Div24>
+              {fetchError && (<p>{fetchError}</p>)}
+              {Lessonscontent && (
+            <div>
+              {Lessonscontent.map(Lessonscontent => (
+              <p>{Lessonscontent.LLS1}</p>
+              ))}
+            </div>
+             )}
+          </Div24>
+          <Div25>
           <Link to="/scio/lessons-learned">
             <Button>More Info...</Button>
           </Link>
+          </Div25>
           </Div23>
           </Div21>
       </Div15>
