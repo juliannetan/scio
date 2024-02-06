fb1321d0f3f090a1ab84db705d085fac0f701cdb
juliannetan <juliannemtan@gmail.com>
2024-02-05 20:54:04 -0700

    added OTP react and refactored authentication pages

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,81 +1,9 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { supabase } from '../components/supabase';
-import styled from 'styled-components';
-import Button from '@mui/material/Button';
-import { styled as muiStyled } from '@mui/system';
-import { Typography, IconButton, InputAdornment, TextField } from '@mui/material';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { Typography, IconButton, InputAdornment } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import Alert from '@mui/material/Alert';
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-image: url(scio_logo2017_dark-blue.png);
-  background-color: #004F71;
-  background-size: 780px 780px;
-  background-position: center;
-  background-repeat: no-repeat;
-`;
-
-const Content = styled.div`
-  text-align: center;
-  max-width: 400px; /* Adjust the width as needed */
-  width: 100%;
-`;
-
-const Title = styled.h2`
-  font-size: 24px;
-  margin-bottom: 60px;
-  color: ${grey[300]};
-`;
-
-const Input = muiStyled(TextField)({
-  width: '100%',
-  marginBottom: '20px',
-  '& label': {
-    color: grey[300],
-  },
-  '& label.Mui-focused': {
-    color: grey[300]
-  },
-  '& input': {
-    color: grey[300],
-  },
-  '& .MuiInput-underline:before': {
-    borderBottomColor: grey[300],
-  },
-  '&&:hover .MuiInput-underline:before': {
-    borderBottomColor: grey[700],
-  },
-  '& .MuiInput-underline:after': {
-    borderBottomColor: grey[700], 
-  },
-});
-
-const ColorButton = muiStyled(Button)({
-  width: '100%',
-  color: grey[300],
-  backgroundColor: grey[500],
-  '&:hover': {
-    backgroundColor: grey[700],
-  },
-  padding: '5px',
-  opacity: 0.9,
-  fontSize: '20px',
-  textTransform: 'capitalize',
-});
-
-const VisibilityIcon = muiStyled(Visibility)({
-  color: grey[300],
-});
-
-const VisibilityOffIcon = muiStyled(VisibilityOff)({
-  color: grey[300],
-});
+import { Container, Title, Content, Input, ColorButton, LinkStyled, VisibilityIcon, VisibilityOffIcon } from './SignInPage';
 
 const SignUpPage = () => {
   const [fullName, setFullName] = useState('');
@@ -201,7 +129,7 @@ const SignUpPage = () => {
         />
         <ColorButton onClick={handleSignUp}>Sign Up</ColorButton>
         <Typography style={{marginTop: '30px', color: grey[500]}}>      
-            Already have an account? <Link to='/scio/' style={{color: grey[300]}}>Login</Link>
+            Already have an account? <LinkStyled to='/scio/'>Login</LinkStyled>
         </Typography>
       </Content>
     </Container>
