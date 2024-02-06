b02bf4bc9edaee6c63d394a5e99193767fb4d97a
juliannetan <juliannemtan@gmail.com>
2024-02-05 18:30:16 -0700

    fixed footer

--- src/pages/MissionPage.js
+++ src/pages/MissionPage.js
@@ -1,22 +1,20 @@
-import React from 'react'
-
-
+import React from 'react';
+import Typography from '@mui/material/Typography';
 
 const MissionPage = () => {
-
   return (
     <main className="App-main">
-    <section>
-      <h2>Our Mission</h2>
-      <p>
-        At Our Company, our mission is to provide high-quality products/services that exceed our customers'
-        expectations. We are committed to innovation, integrity, and excellence in all aspects of our business.
-        Through continuous improvement and a customer-centric approach, we aim to make a positive impact on the
-        communities we serve.
-      </p>
-    </section>
-  </main>
-  )
+      <section style={{ padding: '32px' }}>
+        <Typography variant="h3">Our Mission</Typography>
+        <Typography variant="body1">
+          At Our Company, our mission is to provide high-quality products/services that exceed our customers'
+          expectations. We are committed to innovation, integrity, and excellence in all aspects of our business.
+          Through continuous improvement and a customer-centric approach, we aim to make a positive impact on the
+          communities we serve.
+        </Typography>
+      </section>
+    </main>
+  );
 }
 
-export default MissionPage
+export default MissionPage;
